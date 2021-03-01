6c530536887efb3f18779f76bffa5120e9a6b751
Stef <sstefin@bk.ru>
2021-03-01 11:20:56 -0500

    Added function to extract the last logged ip address

--- src/get_ip.rs
+++ src/get_ip.rs
@@ -1,3 +1,4 @@
+use crate::read_file;
 use chrono::prelude::{DateTime, Local};
 
 // This function will print the Ip Address with time
@@ -10,4 +11,16 @@ pub fn log_ip() -> Result<(), Box<dyn std::error::Error>> {
         local
     );
     Ok(())
+}
+
+pub fn get_last_ip(iplogger_file: &std::path::PathBuf) -> String {
+    let last_line =
+        read_file::read_last_line(iplogger_file.to_str().expect("Cannot Convert to string"))
+            .unwrap();
+    let mut last_ip = String::from("");
+    for l in last_line.split(" - ") {
+        last_ip = String::from(l);
+        break;
+    }
+    last_ip
 }
